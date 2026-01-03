dbf81a93e63127ceb783ede9f923c71da44abff4
Dada Kingsley <Kingsleydada159@gmail.com>
2026-01-03 14:35:44 -0800

    Created Board logic

---
+++ ChessLogic/GameState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessLogic
+{
+    public class GameState
+    {
+        public Board Board { get; }
+        public Player CurrentPlayer { get; private set; }
+
+        public GameState(Player, player, Board board)
+        {
+            CurrentPlayer = player;
+            Board = board;
+        }
+    }
+}
