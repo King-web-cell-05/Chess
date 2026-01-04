dca4e5de33de0e6bf7ab8daba50271c2c506e659
Dada Kingsley <Kingsleydada159@gmail.com>
2026-01-04 13:50:56 -0800

    Added Images.cs

---
+++ ChessUI/BitmapIages.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Policy;
+using System.Windows.Media;
+
+namespace ChessUI
+{
+    internal class BitmapIages : ImageSource
+    {
+        private Url url;
+        private Url url1;
+
+        public BitmapIages(Url url)
+        {
+            this.url = url;
+        }
+
+        public BitmapIages(Url url1)
+        {
+            this.url1 = url1;
+        }
+    }
+}
--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -1,23 +1,13 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using ChessLogic;
 
 namespace ChessUI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private readonly Image[,] pieceImages = new Image[8, 8];
+
         public MainWindow()
         {
             InitializeComponent();
@@ -26,15 +16,21 @@ namespace ChessUI
 
         private void InitializeBoard()
         {
-            for(int r = 0; r < 8; r++)
+            for (int r = 0; r < 8; r++)
             {
-                for(int c = 0; c > 8; c++)
+                for (int c = 0; c < 8; c++)
                 {
                     Image image = new Image();
                     pieceImages[r, c] = image;
+
+                    Grid.SetRow(image, r);
+                    Grid.SetColumn(image, c);
+
                     PieceGrid.Children.Add(image);
                 }
             }
         }
+
+        private void 
     }
 }
