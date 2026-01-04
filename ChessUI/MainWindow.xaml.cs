83c3cdfc89dacdd9697b9e59614b6d2870156a69
Dada Kingsley <Kingsleydada159@gmail.com>
2026-01-04 14:39:11 -0800

    Fixed White piece completion issue

--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -8,10 +8,15 @@ namespace ChessUI
     {
         private readonly Image[,] pieceImages = new Image[8, 8];
 
+        private GameState gameState;
+
         public MainWindow()
         {
             InitializeComponent();
             InitializeBoard();
+
+            gameState = new GameState(Player.White, Board.Initial());
+            DrawBoard(gameState.Board);
         }
 
         private void InitializeBoard()
@@ -31,6 +36,18 @@ namespace ChessUI
             }
         }
 
-        private void 
+        private void DrawBoard(Board board)
+        {
+            for (int r = 0; r < 8; r++)
+            {
+                for (int c = 0; c < 8; c++)
+                {
+                    Piece piece = board[r, c];
+                    pieceImages[r, c].Source = Images.GetImage(piece);
+                }
+            }
+        }
+
+
     }
 }
