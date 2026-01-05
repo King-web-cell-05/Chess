80d90729e88d8bdce1faa2d68f2baf414f2aac38
Dada Kingsley <Kingsleydada159@gmail.com>
2026-01-05 14:12:56 -0800

    Handling moves

--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 using ChessLogic;
 
 namespace ChessUI
@@ -7,8 +10,11 @@ namespace ChessUI
     public partial class MainWindow : Window
     {
         private readonly Image[,] pieceImages = new Image[8, 8];
+        private readonly Rectangle[,] highlights = new Rectangle[8, 8];
+        private readonly Dictionary<Position, Move> moveCache = new Dictionary<Position, Move>();
 
         private GameState gameState;
+        private Position? selectedPos = null;
 
         public MainWindow()
         {
@@ -27,11 +33,16 @@ namespace ChessUI
                 {
                     Image image = new Image();
                     pieceImages[r, c] = image;
-
-                    Grid.SetRow(image, r);
-                    Grid.SetColumn(image, c);
-
                     PieceGrid.Children.Add(image);
+
+                    Rectangle highlight = new Rectangle
+                    {
+                        Fill = Brushes.Transparent,
+                        IsHitTestVisible = false
+                    };
+
+                    highlights[r, c] = highlight;
+                    HighlightGrid.Children.Add(highlight);
                 }
             }
         }
@@ -43,11 +54,36 @@ namespace ChessUI
                 for (int c = 0; c < 8; c++)
                 {
                     Piece piece = board[r, c];
-                    pieceImages[r, c].Source = Images.GetImage(piece);
+                    pieceImages[r, c].Source = piece == null ? null : Images.GetImage(piece);
                 }
             }
         }
 
+        private void CacheMoves(IEnumerable<Move> moves)
+        {
+            moveCache.Clear();
+            foreach (Move move in moves)
+            {
+                moveCache[move.ToPos] = move;
+            }
+        }
 
+        private void ShowHighlights()
+        {
+            Color color = Color.FromArgb(150, 125, 255, 125);
+
+            foreach (Position to in moveCache.Keys)
+            {
+                highlights[to.Row, to.Column].Fill = new SolidColorBrush(color);
+            }
+        }
+
+        private void HideHighlights()
+        {
+            foreach (Position to in moveCache.Keys)
+            {
+                highlights[to.Row, to.Column].Fill = Brushes.Transparent;
+            }
+        }
     }
 }
