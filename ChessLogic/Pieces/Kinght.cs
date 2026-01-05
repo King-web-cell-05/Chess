c7ba485accf3b2a9bc46b94577ebf9d7626bdc46
Dada Kingsley <Kingsleydada159@gmail.com>
2026-01-05 09:25:36 -0800

    Added Moves logic to knight.cs

--- ChessLogic/Pieces/Kinght.cs
+++ ChessLogic/Pieces/Kinght.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ChessLogic.Pieces
+﻿namespace ChessLogic.Pieces
 {
-    public class Knight: Piece
+    public class Knight : Piece
     {
         public override PieceType Type => PieceType.Knight;
         public override Player Color { get; }
@@ -19,6 +15,32 @@ namespace ChessLogic.Pieces
             Knight copy = new Knight(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+
+        private static IEnumerable<Position> PotentialToPositions(Position from)
+        {
+            foreach (Direction vDir in new[] { Direction.North, Direction.South })
+            {
+                foreach (Direction hDir in new[] { Direction.West, Direction.East })
+                {
+                    yield return from + 2 * vDir + hDir;
+                    yield return from + 2 * hDir + vDir;
+                }
+            }
+        }
+
+        private IEnumerable<Position> MovePositions(Position from, Board board)
+        {
+            return PotentialToPositions(from)
+                .Where(pos =>
+                    Board.IsInside(pos) &&
+                    (board.IsEmpty(pos) || board[pos].Color != Color));
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return MovePositions(from, board)
+                .Select(to => new NormalMove(from, to));
         }
     }
 }
