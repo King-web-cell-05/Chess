89bcb57613086860a46e6dcdb13367d27e6f1a1d
Dada Kingsley <Kingsleydada159@gmail.com>
2026-01-18 20:46:25 -0800

    Creating Statestring for Pieceplacement and currentplayer

---
+++ ChessLogic/StateString.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace ChessLogic
+{
+    public class StateString
+    {
+        private readonly StringBuilder sb = new StringBuilder();
+
+        public StateString(Player currentPlayer, Board board) 
+        {
+            AddPieceplacement(board);
+            sb.Append(' ');
+            AddCurrentPlayer(currentPlayer);
+            sb.Append(' ');
+            //Add castling rights
+            sb.Append(" ");
+            //Add en passant data
+
+
+        }
+
+        private static char PieceChar(Piece piece)
+        {
+            char c = piece.Type switch
+            {
+                PieceType.Pawn => 'p',
+                PieceType.Knight => 'n',
+                PieceType.Rook => 'r',
+                PieceType.Bishop => 'b',
+                PieceType.Queen => 'q',
+                PieceType.King => 'k',
+                _ => ' '
+
+            };
+
+            if (piece.Color == Player.White)
+            {
+                return char.ToUpper(c);
+            }
+
+            return c;
+        }
+
+        private void AddRowData(Board board, int row)
+        {
+            int empty = 0;
+
+            for (int c = 0; c < 8; c++)
+            {
+                if (board[row, c]  == null)
+                {
+                    empty++;
+                    continue;
+                }
+
+                if (empty > 0)
+                {
+                    sb.Append(empty);
+                    empty = 0;
+                }
+
+                sb.Append(PieceChar(board[row, c]));
+            }
+            if (empty > 0)
+            {
+                sb.Append(empty);
+            }
+        }
+
+        private void AddPieceplacement(Board board)
+        {
+            for (int r = 0; r < 8; r++)
+            {
+                if (r != 0)
+                {
+                    sb.Append('/');
+                }
+
+                AddRowData(board, r);
+            }
+        }
+
+        private void AddCurrentPlayer(Player currentPlayer)
+        {
+            if (currentPlayer == Player.White)
+            {
+                sb.Append('w');
+            }
+            else
+            {
+                sb.Append('b');
+            }
+        }
+
+    }
+}
